269f04251dd5c13d2da9df07794f76ee1e61f647
Farhan Rahman <92721094+frahman28@users.noreply.github.com>
2022-03-06 04:08:50 +0000

    Basic algorithm

--- basicBackProp.py
+++ basicBackProp.py
@@ -4,34 +4,46 @@ import math
 class Network:
     learnRate = 0.5
 
+    #initialise network with number of inputs, number of hidden nodes and weights and bias' can be assigned if desired
     def __init__(self, no_inputs, no_hidden, weightsH = None, weightsO = None, biasH = None, biasO = None):
         self.noOfInputs = no_inputs
 
-        self.layerHidden = HiddenLayer(no_hidden, biasH)
-        self.setWeightsH(weightsH)
+        self.layerHidden = Layer(no_hidden, biasH) #set hidden layer object with inputted number of hidden nodes and bias if assigned
+        self.nodeOutput = OutputNode(biasO) #set output node object with inputted bias if assigned
+        
+        self.setWeightsH(weightsH) #call function to set weights between inputs and hidden layer, set to assigned weights if inputted
+        self.setWeightsO(weightsO) #call function to set weights between hidden layer and output node, set to assigned weights if inputted
 
-        def setWeightsH(self, weightsH):
+        def setWeightsH(self, weightsH): #loop through each hidden node and append a weight to node's weight list for each input
             weight = 0
-            for i in range(len(self.layerH)):
+            for i in range(len(self.layerH)): 
                 for j in range(self.noOfInputs):
                     if not weightsH:
                         self.layerHidden.nodes[i].weights.append(random.random())
                     else:
                         self.layerHidden.nodes[i].weights.append(weightsH[weight])
                     weight += 1
+        
+        def setWeightsO(self, weightsO): #loop through each hidden node and append weight to output node weight list
+            weight = 0
+            for i in range(len(self.layerH)):
+                if not weightsO:
+                    self.nodeOutput.weights.append(random.random())
+                else:
+                    self.nodeOutput.weights.append(weightsO[weight])
+                weight += 1
 
-        def forwardPass(self, inputs):
+        def forwardPass(self, inputs): #use activation functions to make forward pass through network
+            return self.nodeOutput.activation(self.layerHidden.forwardPass(inputs))
 
         def train(self, inputs, output):
             self.forwardPass(inputs)
 
-
-
-
+            
 
     
 
-def Layer:
+class Layer:
     def __init__(self, no_nodes, bias):
         self.bias = bias if bias else random.random() 
         self.nodes = []
@@ -71,7 +83,7 @@ class Node:
 
 class OutputNode:
     def __init__(self, bias):
-        self.bias = bias #initialise output node with bias
+        self.bias = bias if bias else random.random() #initialise output node with bias
         self.weights = []
 
     def activation(self, inputs):
@@ -84,5 +96,3 @@ class OutputNode:
     
     def delta(self, correctOutput):
         return (correctOutput-self.output)*(self.output*(1-self.output))
-    
-    
