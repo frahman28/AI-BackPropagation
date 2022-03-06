8c72f4af40969ef0c61e6d43e3393c032b276675
Farhan Rahman <92721094+frahman28@users.noreply.github.com>
2022-03-06 17:44:38 +0000

    Basic algorithm

--- basicBackProp.py
+++ basicBackProp.py
@@ -2,13 +2,13 @@ import random
 import math
 
 class Network:
-    learnRate = 0.5
+    learnRate = 0.1
 
     #initialise network with number of inputs, number of hidden nodes and weights and bias' can be assigned if desired
     def __init__(self, no_inputs, no_hidden, weightsH = None, weightsO = None, biasH = None, biasO = None):
         self.noOfInputs = no_inputs
 
-        self.layerHidden = Layer(no_hidden, biasH) #set hidden layer object with inputted number of hidden nodes and bias if assigned
+        self.layerHidden = HiddenLayer(no_hidden, biasH) #set hidden layer object with inputted number of hidden nodes and bias if assigned
         self.nodeOutput = OutputNode(biasO) #set output node object with inputted bias if assigned
         
         self.setWeightsH(weightsH) #call function to set weights between inputs and hidden layer, set to assigned weights if inputted
@@ -39,11 +39,27 @@ class Network:
         def train(self, inputs, output):
             self.forwardPass(inputs)
 
-            
+            #Delta for output node
+            outputDelta = self.nodeOutput.delta(output)
 
+            #Delta for hidden nodes
+            hiddenDeltas = [0]*len(self.layerHidden.nodes)
+            for i in range(len(self.layerHidden.nodes)):
+                hiddenDeltas[i] = self.layerHidden.nodes[i].derivative(output)*outputDelta*self.nodeOutput.weights[i]
+            
+            #Update weights for hidden to output node
+            for i in range(len(self.nodeOutput.weights)):
+                self.nodeOutput.weights[i] += self.learnRate*outputDelta*self.nodeOuput.output
+
+            #Update weights for input to hidden
+            for i in range(len(self.layerHidden.nodes)):
+                for j in range(len(self.layerHidden.nodes[i].weights)):
+                    self.layerHidden.nodes[i].weights[j] += self.learnRate*hiddenDeltas[i]*self.layerHidden.nodes[i].output
+
+        def 
     
 
-class Layer:
+class HiddenLayer:
     def __init__(self, no_nodes, bias):
         self.bias = bias if bias else random.random() 
         self.nodes = []
@@ -75,8 +91,8 @@ class Node:
         self.output = 1/(1+math.exp(-(total+self.bias))) #use sigmoid function to calculate output
         return self.output
     
-    def error(self, correctOutput): #calculate node error using mean square
-        return 0.5*(correctOutput-self.output)**2
+    def error(self, correctOutput): #calculate node error 
+        return correctOutput-self.output
 
     def derivative(self, correctOutput):
         return self.output*(1-self.output)
@@ -96,3 +112,5 @@ class OutputNode:
     
     def delta(self, correctOutput):
         return (correctOutput-self.output)*(self.output*(1-self.output))
+    
+    def error(self, correctOutput)
